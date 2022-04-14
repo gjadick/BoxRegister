cf6d21e95544fb76d6c9bda5dadb9842222aeecb
Gia Jadick <giajadick@gmail.com>
2022-04-14 16:04:06 -0500

    updated main to run with different file extensions (default is now .tif)

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ from PIL import Image
 from CornerReg import get_corners, rotate_crop
 
 
-r_thresh = 0.05     # threshold for the edges (default 5%)
-fdir = './imgs'     # path to the .jpg images 
+r_thresh = 0.5      # threshold for the edges (default 5%)
+fdir = './films'    # path to the images 
+fext = '.tif'       # file extension for the images
 plot_check = True   # whether to plot original vs. final images
 
 
 if __name__ == '__main__':
 
-    files = [os.path.join(fdir, fname) for fname in os.listdir(fdir) if '.jpg' in fname]
-
+    files = [os.path.join(fdir, fname) for fname in os.listdir(fdir) if fext in fname]
+    
     films = []
     for fname in files:
         print(fname)
@@ -31,7 +32,7 @@ if __name__ == '__main__':
         arr = np.mean(np.asarray(img), axis=2)/2.55  # 0 to 100
     
         # process image
-        xc, yc = get_corners(img, r_thresh)
+        xc, yc = get_corners(img, r_thresh, show_plot=False)
         arr_final = rotate_crop(img, xc, yc)
         films.append(arr_final)
 
@@ -41,6 +42,7 @@ if __name__ == '__main__':
             fig,ax=plt.subplots(1,2,figsize=[8,4], dpi=150)
             ax[0].imshow(arr, cmap='gray')
             ax[0].set_title('Original image')
+            ax[0].plot(np.tile(xc,2), np.tile(yc,2), markersize=10, color='r', lw=1, marker='x')
             ax[1].imshow(arr_final, cmap='gray')
             ax[1].set_title(f'Final image {Nx}x{Ny}')
             plt.show()
